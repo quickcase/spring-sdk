fd42f51e171c1fb9d5c706c897aa922f918719ea
Valentin Laurin <valentin.laurin@quickcase.app>
2023-03-11 22:49:31 +0000

    Add dependency on Jackson databind 2.14.2
    Used for handling of JSON data

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -8,11 +8,7 @@ repositories {
 }
 
 dependencies {
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api("org.apache.commons:commons-math3:3.6.1")
-
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation("com.google.guava:guava:31.1-jre")
+    api("com.fasterxml.jackson.core:jackson-databind:2.14.2")
 }
 
 testing {
