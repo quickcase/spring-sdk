d0f260a659f6e69c2b389dc2ebadc00a9c6b3c09
Valentin Laurin <valentin.laurin@quickcase.app>
2025-01-27 17:04:57 +0000

    Bump jackson-databind from 2.14.2 to 2.18.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 }
 
 dependencies {
-    api("com.fasterxml.jackson.core:jackson-databind:2.14.2")
+    api("com.fasterxml.jackson.core:jackson-databind:2.18.2")
 
     testImplementation("org.hamcrest:hamcrest:2.2")
 }
