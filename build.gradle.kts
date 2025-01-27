a0c6eff15ed851cd59d8027d215614a421e7fc8a
Valentin Laurin <valentin.laurin@quickcase.app>
2025-01-27 17:00:29 +0000

    Bump JUnit from 5.9.1 to 5.11.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
 testing {
     suites {
         val test by getting(JvmTestSuite::class) {
-            useJUnitJupiter("5.9.1")
+            useJUnitJupiter("5.11.4")
         }
     }
 }
