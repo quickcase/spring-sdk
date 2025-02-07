70e5fe181de5303d2f6673f01c8e9ea68fbe651a
Valentin Laurin <valentin.laurin@quickcase.app>
2025-02-07 09:45:22 +0000

    Add global field path validation
    Defers validation to all possible sub-classes

--- src/test/java/app/quickcase/sdk/spring/path/FieldPathTest.java
+++ src/test/java/app/quickcase/sdk/spring/path/FieldPathTest.java
@@ -4,6 +4,8 @@ import app.quickcase.sdk.spring.metadata.Metadata;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -11,6 +13,55 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class FieldPathTest {
+
+    @Nested
+    class Accepts {
+        @ParameterizedTest
+        @ValueSource(strings = {
+                // metadata
+                "[type]",
+                "[state]",
+                // data field
+                "field1",
+                "another_field",
+                "complex1.member1",
+                "collection1[].value.member1",
+                "collection1[0].value.member1",
+                "collection1[id:item_1].value.member1",
+                // computed field
+                ":backlinksCount",
+                ":another_computed_field",
+        })
+        @DisplayName("should accept valid field path")
+        void shouldAcceptValidFieldPath(String path) {
+            assertThat(FieldPath.accepts(path), is(true));
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {
+                // metadata
+                "[invalidMeta]",
+                "[state",
+                "state]",
+                // data field
+                "fie:ld1",
+                "another field",
+                ".field2",
+                "complex1..member1",
+                "collection1[abc].value.member1",
+                "[0]collection1.value.member1",
+                "collection1[id:item_1]value.member1",
+                // computed field
+                ":",
+                ":computed:",
+                ":com:puted",
+                ":computed[0]",
+        })
+        @DisplayName("should reject invalid field path")
+        void shouldRejectInvalidFieldPath(String path) {
+            assertThat(FieldPath.accepts(path), is(false));
+        }
+    }
 
     @Nested
     class OfMetadata {
