293498a38c0ba58955b956e93ca1ee8f40a74eba
Valentin Laurin <valentin.laurin@quickcase.app>
2025-03-13 22:40:10 +0000

    Auto-configure logging beans

--- src/main/java/app/quickcase/sdk/spring/logging/LoggingAutoConfiguration.java
+++ src/main/java/app/quickcase/sdk/spring/logging/LoggingAutoConfiguration.java
@@ -1,6 +1,7 @@
 package app.quickcase.sdk.spring.logging;
 
 import jakarta.servlet.Filter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -11,22 +12,31 @@ import org.springframework.core.annotation.Order;
  * access logs and request tracing.
  */
 @Configuration
-public class LoggingConfiguration {
+public class LoggingAutoConfiguration {
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
+    @ConditionalOnMissingBean(MDCBoundaryFilter.class)
     public Filter mdcBoundaryFilter() {
         return new MDCBoundaryFilter();
     }
 
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE + 1)
+    @ConditionalOnMissingBean(TraceRequestFilter.class)
     public Filter traceRequestFilter() {
         return new TraceRequestFilter();
     }
 
     @Bean
+    @ConditionalOnMissingBean(AccessLogLevelStrategy.class)
+    public AccessLogLevelStrategy defaultAccessLogLevelStrategy() {
+        return new DefaultAccessLogLevelStrategy();
+    }
+
+    @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE + 2)
-    public Filter accessLogFilter() {
-        return new AccessLogFilter();
+    @ConditionalOnMissingBean(AccessLogFilter.class)
+    public Filter accessLogFilter(AccessLogLevelStrategy accessLogLevelStrategy) {
+        return new AccessLogFilter(accessLogLevelStrategy);
     }
 }
